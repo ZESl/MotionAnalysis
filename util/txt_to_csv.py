cff4acbc7efc3d3b12b5210f18814584165ed3c4
ZESl <823989065@qq.com>
2021-04-04 13:34:14 +0800

    假如有一个设备断连了，则判定这条数据作废。

--- util/txt_to_csv.py
+++ util/txt_to_csv.py
@@ -19,20 +19,39 @@ def convert_txt_csv(txt_path, csv_path, pid, tid):
             rightController = string[2][17:-1].split(",")
             leftTracker = string[3][13:-1].split(",")
             rightTracker = string[4][14:-1].split(",")
+
+            lc_x = leftController[0].strip(" ")
+            lc_y = leftController[1].strip(" ")
+            lc_z = leftController[2].strip(" ")
+            rc_x = rightController[0].strip(" ")
+            rc_y = rightController[1].strip(" ")
+            rc_z = rightController[2].strip(" ")
+            lt_x = leftTracker[0].strip(" ")
+            lt_y = leftTracker[1].strip(" ")
+            lt_z = leftTracker[2].strip(" ")
+            rt_x = rightTracker[0].strip(" ")
+            rt_y = rightTracker[1].strip(" ")
+            rt_z = rightTracker[2].strip(" ")
+
+            # 假如有一个设备断连了，则这条数据作废
+            if (lc_x == 0 and lc_y == 0 and lc_z == 0) or (rc_x == 0 and rc_y == 0 and rc_z == 0) or \
+                    (lt_x == 0 and lt_y == 0 and lt_z == 0) or (rt_x == 0 and rt_y == 0 and rt_z == 0):
+                continue
+
             # ["time", "lc_x", "lc_y", "lc_z", "rc_x", "rc_y", "rc_z", "lt_x", "lt_y", "lt_z", "rt_x", "rt_y", "rt_z"]
             data = [{"time": string[0],
-                     "lc_x": leftController[0].strip(" "),
-                     "lc_y": leftController[1].strip(" "),
-                     "lc_z": leftController[2].strip(" "),
-                     "rc_x": rightController[0].strip(" "),
-                     "rc_y": rightController[1].strip(" "),
-                     "rc_z": rightController[2].strip(" "),
-                     "lt_x": leftTracker[0].strip(" "),
-                     "lt_y": leftTracker[1].strip(" "),
-                     "lt_z": leftTracker[2].strip(" "),
-                     "rt_x": rightTracker[0].strip(" "),
-                     "rt_y": rightTracker[1].strip(" "),
-                     "rt_z": rightTracker[2].strip(" ")}]
+                     "lc_x": lc_x,
+                     "lc_y": lc_y,
+                     "lc_z": lc_z,
+                     "rc_x": rc_x,
+                     "rc_y": rc_y,
+                     "rc_z": rc_z,
+                     "lt_x": lt_x,
+                     "lt_y": lt_y,
+                     "lt_z": lt_z,
+                     "rt_x": rt_x,
+                     "rt_y": rt_y,
+                     "rt_z": rt_z}]
             df = df.append(data, ignore_index=True)
         # print(df)
 
