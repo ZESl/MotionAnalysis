d6c7be0fdd06a96616d16d51105b28cebbf07a11
ZESl <823989065@qq.com>
2021-04-11 21:39:09 +0800

    ripper update

--- RIPPER/ripper.py
+++ RIPPER/ripper.py
@@ -11,16 +11,15 @@ from sklearn.metrics import roc_auc_score
 
 pd.set_option("chained_assignment", None)
 data_frame = pd.read_csv("../Dataset/RIPPER_half.csv")
-# Part a
 
+# Part a
 data = data_frame[["side", "event", "trial", "gender", "fre_side", "VR_exp", "game_fre", "sport_fre", "cut_mean"]]
 
 # Part b
-
 # data.age = data.age.apply(lambda x: "younger person" if x <= 55 else "older person")
+count = len(data.columns)
 
 # Part c
-
 label_encoder = preprocessing.LabelEncoder()
 categorical_columns = data.select_dtypes(['object']).columns
 for i in range(len(categorical_columns)):
@@ -28,8 +27,7 @@ for i in range(len(categorical_columns)):
 # print(data[categorical_columns])
 
 # Part d
-# todo 改data.columns[0:？]
-dataset = data[data.columns[0:9]]
+dataset = data[data.columns[0:count]]
 target = data["cut_mean"]
 ripper_dataset = data
 x_train, x_test, y_train, y_test = train_test_split(dataset, target, test_size=0.2, random_state=123)
@@ -40,8 +38,6 @@ print("Train size for Ripper" + " ------------>  " + str(ripper_train.shape))
 print("Test size for Ripper" + "  ------------>  " + str(ripper_test.shape))
 
 # Part e
-
-# todo ripper_test.columns[？]
 print("")
 print("Ripper Algorithm")
 ripper_clf = lw.RIPPER()
@@ -49,11 +45,10 @@ ripper_start_time = time.time()
 ripper_clf.fit(ripper_train, class_feat="cut_mean", random_state=123)
 ripper_predict = ripper_clf.predict(ripper_test)
 ripper_run_time = time.time() - ripper_start_time
-print("Accuracy with Ripper: ", accuracy_score(ripper_test[ripper_test.columns[8]], ripper_predict))
+print("Accuracy with Ripper: ", accuracy_score(ripper_test[ripper_test.columns[count - 1]], ripper_predict))
 print(ripper_clf.ruleset_.out_pretty())
 
 # Part f
-
 print("")
 print("Decision Tree with Entropy")
 entropy_data = DecisionTreeClassifier(criterion="entropy", random_state=123, max_depth=5)
@@ -64,8 +59,7 @@ entropy_run_time = time.time() - entropy_start_time
 print("Accuracy with Entropy: ", accuracy_score(y_test, entropy_pred_data))
 
 # Part g
-# todo ripper_test.columns[？]
-auc_ripper = roc_auc_score(ripper_test[ripper_test.columns[8]], ripper_predict)
+auc_ripper = roc_auc_score(ripper_test[ripper_test.columns[count - 1]], ripper_predict)
 auc_entropy = roc_auc_score(y_test, entropy_pred_data)
 print("")
 print("AUC value for Ripper" + " ------------>  " + str(auc_ripper))
