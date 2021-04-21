63f9a14dc80694b4228af3a6140588b025b63d10
ZESl <823989065@qq.com>
2021-04-21 21:01:57 +0800

    更改event 删掉单手反手远距离这一事件

--- data_event/getVRevent.py
+++ data_event/getVRevent.py
@@ -16,12 +16,12 @@ for note in notes:
         event = 2
     elif (note['_lineIndex'] == 2 and note['_type'] == 0) or (note['_lineIndex'] == 1 and note['_type'] == 1):
         event = 3
-    elif (note['_lineIndex'] == 3 and note['_type'] == 0) or (note['_lineIndex'] == 0 and note['_type'] == 1):
-        event = 4
+    # elif (note['_lineIndex'] == 3 and note['_type'] == 0) or (note['_lineIndex'] == 0 and note['_type'] == 1):
+    #     event = 4
 
     if t == pre:
         tins.pop()
-        event = 5
+        event = 4
     time = (t * 60 / 108) + 1  # 加上方块过来的时间
     tins.append([time, event])
     pre = t
